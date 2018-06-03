2ac9c8a8ad6f4c3318964a1da413485a8fa6bac2
paveldemidyuk <3128914@gmail.com>
2018-06-03 22:51:00 +0300

    debug

--- models/device.js
+++ models/device.js
@@ -106,9 +106,6 @@ getRandomInt = () => Math.floor(Math.random() * Math.floor(1000000));
 
 var getAllDevicesData = function (blacklist) {
     return dirall.call(client, '/').then(function (entries) {
-
-        DEBUG(entries)
-
         return Promise.filter(entries, function (entry) {
             return !blacklist || !entry.match(blacklist);
         }).then(function (entries) {
