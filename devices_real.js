a1c89eaf7d34f8388476140caed2443991138d82
paveldemidyuk <3128914@gmail.com>
2018-06-04 16:51:53 +0300

    debug

--- devices_real.js
+++ devices_real.js
@@ -5,14 +5,14 @@ var devices = [
         name: 'первый рубильник',
         type: 'switcher',
         switcher_path: '/3A.B7C507000000/PIO.A',
-        sensor_path: '/3A.9FC307000000/PIO.B'
+        sensor_path: '/3A.9FC307000000/PIO.A'
     },
 
     {
         name: 'второй рубильник',
         type: 'switcher',
-        switcher_path: '/3A.B7C507000000/PIO.A',
-        sensor_path: '/3A.9FC307000000/PIO.A'
+        switcher_path: '/3A.B7C507000000/PIO.B',
+        sensor_path: '/3A.9FC307000000/PIO.B'
     },
     {
         name: 'термометр',
