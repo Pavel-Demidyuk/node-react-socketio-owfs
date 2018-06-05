2332c277bf9455a2897d97496cff0cec56881c47
paveldemidyuk <3128914@gmail.com>
2018-06-05 16:12:34 +0300

    debug

--- devices_real.js
+++ devices_real.js
@@ -5,14 +5,14 @@ var devices = [
         name: 'рекуператор',
         type: 'switcher',
         switcher_path: '/3A.EEC507000000/PIO.A',
-        sensor_path: '/3A.7DC607000000/PIO.A'
+        sensor_path: '/3A.7DC607000000/SENSED.A'
     },
 
     {
         name: 'теплый пол',
         type: 'switcher',
         switcher_path: '/3A.EEC507000000/PIO.B',
-        sensor_path: '/3A.7DC607000000/PIO.B'
+        sensor_path: '/3A.7DC607000000/SENSED.B'
     },
     {
         name: 'Рекуператор C°',
--- models/device.js
+++ models/device.js
@@ -2,8 +2,8 @@ var Promise = require('bluebird');
 
 var devicesConfig = require('../devices_real.js');
 
-var Client = require("owfs").Client; // real client !! DO NOT REMOVE!!
-// var Client = require('../models/fake_owfs').Client;
+// var Client = require("owfs").Client; // real client !! DO NOT REMOVE!!
+var Client = require('../models/fake_owfs').Client;
 
 var dirall = Promise.promisify(
     Client.prototype.dirall
@@ -26,49 +26,8 @@ function getRandomInt() {
     return Math.floor(Math.random() * Math.floor(10000000));
 }
 
-//
-// var read = function (device) {
-//     devicePath = checkFormat(device.path);
-//     return Promise.all([
-//         readFullPath(devicePath + '/' + 'PIO.ALL'),
-//         readFullPath(devicePath + '/' + 'PIO.A'),
-//         readFullPath(devicePath + '/' + 'PIO.B'),
-//         readFullPath(devicePath + '/' + 'sensed.ALL'),
-//         readFullPath(devicePath + '/' + 'sensed.A'),
-//         readFullPath(devicePath + '/' + 'sensed.B')
-//     ]).then(function (data) {
-//         return new Promise(function (resolve, reject) {
-//             if (typeof data[0] == 'undefined') {
-//                 // thermo
-//                 resolve(
-//                     {
-//                         device: devicePath,
-//                         data: data[6],
-//                     }
-//                 );
-//             }
-//             else {
-//                 // switcher
-//                 resolve(
-//                     {
-//                         device: devicePath,
-//                         'PIO.ALL': data[0],
-//                         'PIO.A': data[1],
-//                         'PIO.B': data[2],
-//                         'sensed.ALL': data[3],
-//                         'sensed.A': data[4],
-//                         'sensed.B': data[5],
-//                     }
-//                 );
-//             }
-//         })
-//     })
-// };
-
 var readFullPath = function (fullPath) {
     fullPath = checkFormat(fullPath);
-
-
     return new Promise(function (resolve, reject) {
         client.read(fullPath, function (err, data) {
             DEBUG_OWFS("FULL PATH", fullPath, data);
@@ -94,16 +53,6 @@ var write = function (fullPath, value) {
         });
     });
 };
-
-var getAllDevicesData = function (blacklist) {
-    return dirall.call(client, '/').then(function (entries) {
-        return Promise.filter(entries, function (entry) {
-            return !blacklist || !entry.match(blacklist);
-        }).then(function (entries) {
-            return Promise.all(entries.map(read, {concurrency: concurrency}));
-        });
-    });
-}
 
 var device = {
     getRaw: function (callback) {
