4e9e81c3f1d263f87dc14c75d98d48ca461e594e
paveldemidyuk <3128914@gmail.com>
2018-06-06 14:13:43 +0300

    update

--- configs/config.js
+++ configs/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    devices_refresh_interval : 2000, // частота принудительного обновления выключателей (милисекунды)
-    thermos_refresh_interval : 3000, // частота принудительного обновления термометров (милисекунды)
+    devices_refresh_interval : 3000, // частота принудительного обновления выключателей (милисекунды)
+    thermos_refresh_interval : 5000, // частота принудительного обновления термометров (милисекунды)
     thermo_display_limit : 15, // колво отображаемых последних температур
     server_host: 'raspberrypi.local',
     server_port : 4001
